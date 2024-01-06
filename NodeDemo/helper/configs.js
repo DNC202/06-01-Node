a184a93b3416868ef6b50b623acb66a59bf40162
Danh <nguyencongdanh2002@gmail.com>
2024-01-06 22:52:59 +0700

    forgot password

---
+++ NodeDemo/helper/configs.js
@@ -0,0 +1,11 @@
+module.exports= {
+    SECRET_KEY:"heheheehe",
+    EXP: '20d',
+    cookieExp: 2,
+    
+    MAIL_HOST:'smtp.gmail.com',
+    MAIL_PORT: 465,
+    MAIL_USER: 'nguyencongdanh2002@gmail.com',
+    MAIL_PASS:'ofes mspq jdef appq',
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Ho va ten: Le Anh Tuan
+Mssv: 2011061245
